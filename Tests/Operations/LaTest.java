d4c28141967815fcc0e9a0e8e3a473808bfe7fd6
JosiahDeYong <jpdeyong@uwm.edu>
2024-03-12 12:18:31 -0500

    Fiexd push to wrong folder, basic LaTest

--- Tests/Operations/LaTest.java
+++ Tests/Operations/LaTest.java
@@ -1,7 +1,26 @@
 package Operations;
 
-import static org.junit.jupiter.api.Assertions.*;
-// Josiah
-class LaTest {
+import org.junit.jupiter.api.Test;
 
+import javax.annotation.processing.SupportedAnnotationTypes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//  lui $at, 4097 (0x1001 → upper 16 bits of $at).
+//  ori $a0,$at,disp
+public class LaTest {
+    String[] instr = {"la", "$s0", "4000"};
+    String[] instr_hex = {"la", "$s0", "0x12345678"};
+    //LI - LUI - Copy upper 1/2 bits, ORI copy lower 1/2 bits
+    @Test
+    public void testRegularSuccess()
+    {
+
+    }
+
+    @Test
+    public void testHexSuccess()
+    {
+
+    }
 }
