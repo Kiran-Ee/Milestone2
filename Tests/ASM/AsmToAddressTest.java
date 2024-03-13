cc0d01fc565f237744b816e1f86000dd32ee4b43
Tanja <tcapps@uwm.edu>
2024-03-12 22:04:46 -0500

    asmToAddTests

---
+++ Tests/ASM/AsmToAddressTest.java
@@ -0,0 +1,36 @@
+package ASM;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AsmToAddressTest {
+    DataSection data_sec = new DataSection();
+
+    String empty = "";
+    String comm = "#comment";
+    String asm = ".data var: .word 42, var1: .asciiz Request /n.text main: add $t0, $t1, $t2";
+    String text = ".text main: add $t0, $t1, $t2";
+    String[] emptyStr = new String[]{"", ""};
+    String[] comment1 = {"", ""};
+    String[] asm1 = new String[]{"0x10010000 2A00 10010004 68656C6C6F", "00400000 21080008 014B4820"};//need add. check
+    String[] text1 = new String[]{"", "00400000 21080008 014B4820"}; //add check,
+
+    @Test
+    void emptyString(){
+        Assertions.assertEquals(emptyStr, data_sec.asm_to_address(empty));
+    }
+    @Test
+    void comment(){
+        Assertions.assertEquals(comment1, data_sec.asm_to_address(comm));
+    }
+    @Test
+    void asm(){
+        Assertions.assertEquals(asm1, data_sec.asm_to_address(asm));
+    }
+    @Test
+    void text_section(){
+        Assertions.assertEquals(text1, data_sec.asm_to_address(text));
+    }
+}
--- src/ASM/DataSection.java
+++ src/ASM/DataSection.java
@@ -14,6 +14,7 @@ public class DataSection {
     public String calc_next_address(String prev_address, int current_size) {
         return null;
     }
+    public String[] asm_to_address(String asm)   {return null;}
     public String[] asm_parser(String asm)  {return null;}
     public String[] address_factory(boolean isData, String sec)   {return null;}
     public String[] declaration_line_cleaner(String line) {return null;
