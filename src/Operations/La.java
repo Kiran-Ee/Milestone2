b5bb10e6314bcff5769674373c90284d92d1371a
JosiahDeYong <jpdeyong@uwm.edu>
2024-03-26 00:53:03 -0500

    Reviewed code, should be done

--- src/Operations/La.java
+++ src/Operations/La.java
@@ -6,8 +6,9 @@ public class La implements PseudoOperation {
     private Lui luihalf;
     private Ori orihalf;
     public La(String[] cleaned_instructions){ //EX: "la, "$t0", "10010000"
-        String[] lui = new String[]{"Lui", "$at", "4097"}; //Will always be 0x1001, because identifier for data address
-        String[] ori = new String[]{"Ori", cleaned_instructions[1], "$at", "0x" + cleaned_instructions[2].substring(4)};
+        String[] lui = new String[]{"lui", "$at", "4097"}; //Will always be 0x1001, because identifier for data address
+        //Construct ori, parameter needs to be in hex, code gets last 4 bits
+        String[] ori = new String[]{"ori", cleaned_instructions[1], "$at", "0x" + cleaned_instructions[2].substring(4)};
         luihalf = new Lui(lui);
         orihalf = new Ori(ori);
         originalInstruction = cleaned_instructions;
