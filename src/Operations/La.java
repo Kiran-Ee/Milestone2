5e3ab86050c093e8517306b339b05c8033265363
JosiahDeYong <jpdeyong@uwm.edu>
2024-03-25 22:24:48 -0500

    LA Should be done now

--- src/Operations/La.java
+++ src/Operations/La.java
@@ -6,10 +6,8 @@ public class La implements PseudoOperation {
     private Lui luihalf;
     private Ori orihalf;
     public La(String[] cleaned_instructions){ //EX: "la, "$t0", "10010000"
-        int shifted_for_lui = Integer.valueOf(cleaned_instructions[2], 16) >>> 4;
-        String[] lui = new String[]{"LUI", "$at", String.valueOf(shifted_for_lui)};
-        int shifted_for_ori = Integer.valueOf(cleaned_instructions[2], 16) << 4 >>> 4;
-        String[] ori = new String[]{"ORI", cleaned_instructions[1], "$at", String.valueOf(shifted_for_ori)};
+        String[] lui = new String[]{"Lui", "$at", "4097"}; //Will always be 0x1001, because identifier for data address
+        String[] ori = new String[]{"Ori", cleaned_instructions[1], "$at", "0x" + cleaned_instructions[2].substring(4)};
         luihalf = new Lui(lui);
         orihalf = new Ori(ori);
         originalInstruction = cleaned_instructions;
